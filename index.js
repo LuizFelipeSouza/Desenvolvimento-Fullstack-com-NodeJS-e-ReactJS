7554081f810acae44c3e0e8f809847eae464d55f
Luiz Felipe Souza <luizsci42@gmail.com>
2018-05-29 17:05:32 -0300

    Faltava um ponto-e-virgula

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
-const express = require('express')
+const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
 	res.send({hi: 'there'});
 });
 
-// const PORT = .listen(process.env.PORT || 5000);
+const PORT = process.env.PORT || 5000;
 app.listen(5000);
